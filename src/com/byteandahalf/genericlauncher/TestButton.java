bbf63e450b3b604169d39064f45f2f3e27fc1c18
derderder <derderder77380@gmail.com>
2016-03-08 22:27:03 +0100

    Update package name

--- src/com/byteandahalf/genericlauncher/TestButton.java
+++ src/com/byteandahalf/genericlauncher/TestButton.java
@@ -6,6 +6,8 @@ import android.graphics.drawable.*;
 import android.view.*;
 import android.widget.*;
 
+import com.derderder.genericlauncher.R;
+
 public class TestButton extends PopupWindow {
 
 	public Button mainButton;
